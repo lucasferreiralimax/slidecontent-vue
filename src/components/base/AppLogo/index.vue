cbba370ef307b9d9a00ca76b7ed8d9b387ba2bdb
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-06 20:55:17 -0300

    adjustmen size logo, region click and title

--- src/components/base/AppLogo/index.vue
+++ src/components/base/AppLogo/index.vue
@@ -8,15 +8,16 @@ const { t } = useI18n();
 <template lang='pug'>
 RouterLink.logo(to='/')
   h1 {{ t('title') }}
-  img(alt='Vue logo' src='@/assets/logo.svg' width='80')
+  img(alt='Vue logo' src='@/assets/logo.svg' width='100')
 </template>
 
 <style lang='stylus'>
 .logo
   text-decoration none
   color #2c3e50
+  display inline-block
   h1
-    font-size 3em
+    font-size 2.5em
     +mobile()
       font-size 1.2em
 </style>
