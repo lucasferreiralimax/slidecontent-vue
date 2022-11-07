1b7cb51e3fbfbafe1bffcd2ec51d689fbe4957ea
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-07 20:24:22 -0300

    test(unit): create expect logo vue and text

--- src/components/base/AppLogo/test.spec.js
+++ src/components/base/AppLogo/test.spec.js
@@ -13,4 +13,14 @@ describe('AppLogo test', () => {
   it('is AppLogo a vue instance', () => {
     expect(wrapper.exists()).toBeTruthy()
   })
+  it('is AppLogo render text SlideContentVue', () => {
+    expect(wrapper.text()).toBe('SlideContentVue')
+  })
+  it('is AppLogo render img logo', () => {
+    const logo = wrapper.find('[alt="Vue logo"]')
+
+    expect(logo).toBeTruthy()
+    expect(logo.element.getAttribute('alt')).toBe('Vue logo')
+    expect(logo.element.getAttribute('width')).toBe('100')
+  })
 })
